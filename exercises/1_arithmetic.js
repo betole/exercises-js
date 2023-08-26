5ae620f2632d18a33d69b0400113880ace630e99
betole <c.mcbottle@gmail.com>
2023-08-26 20:15:03 -0300

    Add exercise 1

---
+++ exercises/1_arithmetic.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+it('Should equal the golden ratio', function() {
+    //Refer to https://es.wikipedia.org/wiki/N%C3%BAmero_%C3%A1ureo or https://en.wikipedia.org/wiki/Golden_ratio
+    const goldenRatio = (1 + ??)/2;
+
+    expect(goldenRatio).to.be.closeTo(1.618, 0.001);
+});
