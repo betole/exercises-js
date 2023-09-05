8aa959fd46f431801b89e9185a2e8abc8fd9794b
betole <c.mcbottle@gmail.com>
2023-09-04 22:47:31 -0300

    Improve exercise 6

--- exercises/6_order_numbers.js
+++ exercises/6_order_numbers.js
@@ -1,52 +1,78 @@
 import {expect} from 'chai'
 
-//This code is supposed to compare numbers, and return an ordered collection, 
-//   from lower to higher or vice versa dependening on the passed order.
-//orderNumber2 should use a different way to order them, and the result of both should be the same
+// This code exposes two functions:
+// A. one to order from lower to higher (orderAscending), 
+// B. the other for the opposite (orderDescending).
+//
+// 1. Fill the gaps in orderNumbersAscending to work as expected.
+// 2. How can orderDescending make use of the existing orderNumbersAscending to give the result in opposite order?
 
-function orderNumbers1(numbers, order) {
+function orderAscending(numbers) {
+    //First, declare empty arrays that will be filled with information
     let result = [];
+    let positions = [];
+    
+    //Iterate over each number, and decide their overall position in the array based on their arithmetic value.
+    for (let i = 0; ___; i++) {
+
+        //Create a filtered array with the rest of the numbers to use later.
+        const number = numbers[i];
+        const otherNumbers = numbers.filter(x => x != ___);
+
+        //Then iterate over this filtered array, comparing the current number with the rest to calculate the position.
+        let position = 0;
+        for (let x = 0; x < ___.length; x++) {
+            if (number ___ otherNumbers[x]) {
+                ___;
+            }
+        }
+        positions.push(position);
+    }
+
+    //Now iterate over the defined positions, to get the ordered number based on the position value.
+    for (let y = ___; y < ___.length; ___) {
+        const orderedIndex = positions.findIndex(position => position === y);
+        const orderedNumber = numbers[orderedIndex];
+        result.push(orderedNumber);
+    }
+
+    return result;
+}
+
+function orderDescending(numbers) {
+    let result = [];
+    const orderedNumbersAscending = orderAscending(numbers);
     //Your code goes here
     return result;
 }
 
-function orderNumbers2(numbers, order) {
-    let result = [];
-    //Your code goes here
-    return result;
-}
-
-describe('orderNumbers', function() {
-    const positiveNumbers = [9,3,15];
-
+describe('Order Numbers', function() {
     [
         {
-            title: 'Order several positive numbers - Ascending',
-            numbers: positiveNumbers,
-            checker: [3,9,15]
-        },
-        {
-            title: 'Order several positive numbers - Descending',
-            numbers: positiveNumbers,
-            checker: [15,9,3]
+            title: 'Order several positive numbers',
+            numbers: [9, 3, 15],
+            ascending: [3, 9, 15],
+            descending: [15, 9, 3]
         },
         {
             title: 'Return same single number',
             numbers: [13],
-            checker: [13]
+            ascending: [13],
+            descending: [13]
         },
         {
-            title: 'Order several mixed numbers - Ascending',
+            title: 'Order several mixed numbers',
             numbers: [2, -7, 0, 32],
-            checker: [-7, 0, 2, 32]
+            ascending: [-7, 0, 2, 32],
+            descending: [32, 2, 0, -7]
         }
     ].forEach(test => {
-        it(test.title + ' method 1', function() {
-            expect(orderNumbers1(test.numbers)).to.equal(test.checker);
+        it(test.title + ' - Ascending Order', function() {
+            expect(orderAscending(test.numbers)).to.deep.equal(test.ascending);
         });
 
-        it(test.title + ' method 2', function() {
-            expect(orderNumbers2(test.numbers)).to.equal(test.checker);
+        it(test.title + '- Descending Order', function() {
+            expect(orderDescending(test.numbers)).to.deep.equal(test.descending);
         });
     });
 });
